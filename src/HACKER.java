569c2fcd05509dd933caad3acb084c18f8fbac85
ErmekWasdDevelop <ermekwasd@gmail.com>
2025-04-09 18:51:16 +0600

    z

--- src/HACKER.java
+++ src/HACKER.java
@@ -14,10 +14,10 @@ public class HACKER extends Hero {
                 for (int i = 0; i < heroes.length; i++) {
                     if (heroes[i].getHealth() > 0 && this != heroes[i]) {
                         if (heroes[a].getHealth() == heroes[i].getHealth()
-                        && heroes[b].getHealth()<=0) {
+                                && heroes[b].getHealth() <= 0) {
                             heroes[a].setHealth(heroes[b].getHealth() + stealHealth);
                         } else if (heroes[b].getHealth() == heroes[i].getHealth()
-                        && heroes[a].getHealth()<=0) {
+                                && heroes[a].getHealth() <= 0) {
                             heroes[b].setHealth(heroes[b].getHealth() + stealHealth);
                         } else {
                             if (stealHealth % 2 == 0) {
@@ -28,12 +28,10 @@ public class HACKER extends Hero {
                                 heroes[b].setHealth(heroes[b].getHealth() + (stealHealth / 2 + 1));
                             }
                         }
+                        System.out.println("HEALTH WAS STEAL :" + stealHealth + "HEALTH");
                     }
                 }
-
             }
-
-            System.out.println("HEALTH WAS STEAL :" + stealHealth + "HEALTH");
         }
     }
 }
